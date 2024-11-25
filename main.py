69be44a6a1b22e596584f32dc4c8442e6c21c197
Mia Ikechukwu <87150395+Mia-IMN@users.noreply.github.com>
2024-11-25 11:49:55 +0100

    Added error handling

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import pandas
 
+# read csv file
 nato_alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
-nato_dictionary = {value.letter:value.code for (key, value) in nato_alphabets.iterrows()}
-# print(nato_dictionary.get("E"))
 
+# create a dictionary variable for the read file
+nato_dictionary = {value.letter:value.code for (key, value) in nato_alphabets.iterrows()}
+
+# getting user input
 user_input = input("Enter a word: ").upper()
-answer = [nato_dictionary.get(word) for word in user_input]
-# or answer = [nato_dictionary[word] for word in user_input]
-print(answer)
+
+try:
+    answer = [nato_dictionary[word] for word in user_input]
+except KeyError:
+    print("Sorry, only letters in the alphabet please.")
+else:
+    print(answer)

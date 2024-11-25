a17deb14fbe03b3ca5e8391addda19d46f81ab4f
Mia Ikechukwu <87150395+Mia-IMN@users.noreply.github.com>
2024-11-25 12:09:22 +0100

    Upgraded code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import final
+
 import pandas
 
 # read csv file
@@ -6,12 +8,19 @@ nato_alphabets = pandas.read_csv("nato_phonetic_alphabet.csv")
 # create a dictionary variable for the read file
 nato_dictionary = {value.letter:value.code for (key, value) in nato_alphabets.iterrows()}
 
-# getting user input
-user_input = input("Enter a word: ").upper()
+on = True
 
-try:
-    answer = [nato_dictionary[word] for word in user_input]
-except KeyError:
-    print("Sorry, only letters in the alphabet please.")
-else:
-    print(answer)
+while on:
+# getting user input
+    user_input = input("Enter a word: ").upper()
+
+    try:
+        answer = [nato_dictionary[word] for word in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(answer)
+        on = False
+
+# I could've defined a function instead of a loop and used recursion on the...
+# except block to get the code to repeat.
